fb35d5a9c77e7f12ba40bcfb9b0fc6d1ba1570ca
Vlad Mnatsakanyan <mnatsakanyan@yual.io>
2025-07-05 15:38:55 +0400

    header

--- components/Header/HeaderMain.vue
+++ components/Header/HeaderMain.vue
@@ -5,37 +5,68 @@
         <img :src="logoSrc" alt="logo" />
       </nuxt-link>
     </div>
-    <div class="header-search">
-      <button 
-        class="burger-menu" 
-        :class="{ 'active': isActive }"
-        @click="toggle"
-        aria-label="Toggle menu"
-      >
-        <span class="burger-line"></span>
-        <span class="burger-line"></span>
-        <span class="burger-line"></span>
-      </button>
 
-      <div class="search-container">
-        <input
-          type="text"
-          class="search-input"
-          placeholder="Поиск товаров..."
-          v-model="query"
-        />
-        <button class="search-btn">Найти</button>
+    <div class="header-right">
+      <div class="header-content">
+        <button 
+          class="burger-menu" 
+          :class="{ 'active': isActive }"
+          @click="toggle"
+          aria-label="Toggle menu"
+        >
+          <span class="burger-line"></span>
+          <span class="burger-line"></span>
+          <span class="burger-line"></span>
+        </button>
+  
+        <div class="search-container">
+          <input
+            type="text"
+            class="search-input"
+            placeholder="Поиск товаров..."
+            v-model="query"
+          />
+          <button class="search-btn">Найти</button>
+        </div>
+  
+        <div class="header-contacts">
+          <nuxt-link to="#">
+            <svgo-telegram />
+          </nuxt-link>
+  
+          <nuxt-link to="#">
+            <svgo-whatsapp />
+          </nuxt-link>
+        </div>
       </div>
 
-      <div class="header-contacts">
+      <div class="header-line">
         <nuxt-link to="#">
-          <svgo-telegram />
+          <svgo-stock />
+          Акции
         </nuxt-link>
 
         <nuxt-link to="#">
-          <svgo-whatsapp />
+          <svgo-lightning />
+          Горячее предложение
+        </nuxt-link>
+
+        <nuxt-link to="#">
+          <svgo-gift />
+          Подарочные наборы
+        </nuxt-link>
+
+        <nuxt-link to="#">
+          <svgo-quest />
+          События
+        </nuxt-link>
+
+        <nuxt-link to="#">
+          <svgo-orc />
+          Наши бренды
         </nuxt-link>
       </div>
+
     </div>
 
     <div class="lang-switcher">
@@ -55,16 +86,27 @@
 
       </div>
     </div>
-    <div class="header-icons">
-      <button>
-        <svgo-like />
-      </button>
-      <button>
-        <svgo-hunter />
-      </button>
-      <button>
-        <svgo-chest />
-      </button>
+
+    <div>
+      <div class="header-icons">
+        <button>
+          <svgo-like />
+        </button>
+        <button>
+          <svgo-hunter />
+        </button>
+        <button>
+          <svgo-chest />
+        </button>
+      </div>
+
+      <div class="header-line header-left">
+        <nuxt-link to="#">
+          <svgo-plus />
+          Собрать компьютер
+        </nuxt-link>
+      </div>
+
     </div>
   </div>
 </template>
